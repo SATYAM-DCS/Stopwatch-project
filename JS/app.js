dbe38043a68b5583a665d76bd6d333f53d1e6987
SATYAM-DCS <dubeysatyam3008@gmail.com>
2022-08-09 19:33:39 +0530

    first commit

---
+++ JS/app.js
@@ -0,0 +1,44 @@
+// inializing the variables 
+var min = 0;
+var sec = 0;
+var miliSec = 0;
+var timer;
+
+// this is the function which is used to update the time and converting according to 
+function callTimer() {
+    miliSec++;
+    if (miliSec < 100) {
+        if (miliSec === 99) {
+            miliSec = 0;
+            sec++;
+            if (sec === 60)  {
+                sec = 0;
+                min++;
+            }
+        }
+    }
+    else{
+        miliSec = 0;
+    }
+    document.getElementById("timer").innerHTML = min + ":" + sec + ":" + miliSec;
+}
+
+// This is the function which is used to start the timer
+function start() {
+    document.getElementById("start").disabled = true;
+    timer = setInterval(callTimer, 10);
+}
+
+//This is the function which is used to stop the timer
+function stop() {
+    document.getElementById("start").disabled = false;
+    clearInterval(timer);
+}
+// This is the function which is used to reset the timer
+function reset() {
+    stop();
+    min = 0;
+    sec = 0;
+    miliSec = 0;
+    document.getElementById("timer").innerHTML = min + ":" + sec + ":" + miliSec;
+}
